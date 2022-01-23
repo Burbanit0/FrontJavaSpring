3e3cafb134707860c4178a15bb971cabdc5b72b3
Burbanit0 <72401931+Burbanit0@users.noreply.github.com>
2022-01-23 15:30:01 +0100

    Add files via upload

---
+++ src/components/developers.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+
+<script>
+export default {
+  name: 'Developers',
+  data() {
+    return {
+      title: 'Developers',
+      msg: ''
+    }
+  },
+  mounted() {
+    fetch("/developers")
+      .then((response) => response.text())
+      .then((data) => {
+          this.msg = JSON.parse(data);
+      });
+  }
+}
+</script>
