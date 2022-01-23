df29fa4aa031d69676f3642a15b9ec69411710a2
Matthieu Burban <matthieuburban@MacBook-Pro-de-Matthieu.local>
2022-01-23 16:12:43 +0100

    projet

--- src/components/developers.vue
+++ src/components/developers.vue
@@ -1,23 +1,59 @@
 <template>
-  <h1>{{ title }}</h1>
+  <div>
+    <h1>{{ title }}</h1>
+    <div>
+        <b-table striped hover :items="developers" :fields="fields"></b-table>
+    </div>
+  </div>
 </template>
 
-
 <script>
+import moment from 'moment';
 export default {
-  name: 'Developers',
-  data() {
-    return {
-      title: 'Developers',
-      msg: ''
+    name: 'Developers',
+    data: function() {
+        return {
+            title: "Developers",
+            fields: [
+                {
+                    key: 'id',
+                    label: 'ID'
+                },
+                {
+                    key: 'firstname',
+                    label: 'First Name'
+                },
+                {
+                    key: 'lastname',
+                    label: 'Last Name'
+                },
+                {
+                    key: 'startContract',
+                    label: 'Start Contract',
+                    formatter: 'startContractFormat'
+                }
+            ],
+        };
+    },
+    computed: {
+        developers: function() {
+            return this.$store.state.developers;
+        }
+    },
+    mounted: function() {
+        this.$store.dispatch('loadDevelopers');
+    },
+    methods: {
+        startContractFormat: function(value) {
+            return moment(value, 'YYYY-MM-DD').format('DD/MM/YYYY');
+        }
     }
-  },
-  mounted() {
-    fetch("/developers")
-      .then((response) => response.text())
-      .then((data) => {
-          this.msg = JSON.parse(data);
-      });
-  }
 }
 </script>
+
+<style scoped>
+h1 {
+  padding: 20px 0;
+  margin: 0;
+}
+</style>
